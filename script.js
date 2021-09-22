15770eda2c3af8646fd18ce9e4e6e7672ab57958
João Victor <joao.lenny100@gmail.com>
2021-09-22 18:50:16 -0300

    Requisito 3 - Lint: ok

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function checkLogin() {
   if (email === 'tryber@teste.com' && password === '123456') {
     alert('Olá, Tryber!');
   } else {
-    alert('Email ou senha inválidos.')
+    alert('Email ou senha inválidos.');
   }
 }
 
