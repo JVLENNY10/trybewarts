92fa920b708eb5b4ad4f5d4ca8562f48a77c9321
João Victor <joao.lenny100@gmail.com>
2021-09-22 18:37:23 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,14 @@
+const btnLogin = document.querySelector('#btn-login');
+
+function checkLogin() {
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+
+  if (email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.')
+  }
+}
+
+btnLogin.addEventListener('click', checkLogin);
